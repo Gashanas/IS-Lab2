6cbe633f1713265489e5bddba8c07310369a5f93
Edvinas Beniušis <gashanas@gmail.com>
2019-12-15 18:48:11 +0200

    refactor: refactored code, removed duplicated lines, added loops

--- lab2.m
+++ lab2.m
@@ -3,35 +3,24 @@ x = 0.1:1/22:1;
 d = (1 + 0.6*sin(2*pi*x/0.7)) + 0.3*sin(2*pi*x)/2;
 plot(x,d, 'kx');
 
-w11 = randn(1);
-w12 = randn(1);
-w13 = randn(1);
-w14 = randn(1);
-w15 = randn(1);
-b11= randn(1);
-b12= randn(1);
-b13= randn(1);
-b14= randn(1);
-b15= randn(1);
-
-w21 = randn(1);
-w22 = randn(1);
-w23 = randn(1);
-w24 = randn(1);
-w25 = randn(1);
+w1 = [randn(1) randn(1) randn(1) randn(1) randn(1)];
+w2 = [randn(1) randn(1) randn(1) randn(1) randn(1)];
+b1 = [randn(1) randn(1) randn(1) randn(1) randn(1)];
 b21= randn(1);
 
 
 e = zeros(1,20);
 y_final = zeros (1,20);
 n= 0.1;
+y = (length(w1));
+delta = (length(w1));
+
 for i=1:length(x)
-    y_1 = 1/(1+exp(-x(i)* w11 - b11));
-    y_2 = 1/(1+exp(-x(i)* w12 - b12));
-    y_3 = 1/(1+exp(-x(i)* w13 - b13));
-    y_4 = 1/(1+exp(-x(i)* w14 - b14));
-    y_5 = 1/(1+exp(-x(i)* w15 - b15));
-    y_final(i) = y_1 * w21 + y_2 * w22 + y_3 * w23 + y_4 * w24 + y_5 * w25 + b21;
+    for j=1:length(w1)
+        y(j) = 1/(1+exp(-x(i)* w1(j) - b1(j)));
+        y_final(i) = y(j) * w2(j) + y_final(i);
+    end
+    y_final(i) = y_final(i) + b21;
     
     % Errors calcualtion
     
@@ -39,36 +28,22 @@ for i=1:length(x)
     
     % Weight update
     % Output layer
-    
-    w21 = w21 + n * e(i)* y_1;
-    w22 = w22 + n * e(i)* y_2;
-    w23 = w23 + n * e(i)* y_3;
-    w24 = w24 + n * e(i)* y_4;
-    w25 = w25 + n * e(i)* y_5;
+    for j=1:length(w1)
+        w2(j) = w2(j) + n * e(i) * y(j);
+    end
     b21 = b21 + n * e(i)* 1;
     
     % Hidden layer
     % deltas
-    delta1 = (1/(1+exp(-x(i)*w11-b11)))*(1-1/(1+exp(-x(i)*w11-b11))) * e(i) * w21;
-    delta2 = (1/(1+exp(-x(i)*w12-b12)))*(1-1/(1+exp(-x(i)*w12-b12))) * e(i) * w22;
-    delta3 = (1/(1+exp(-x(i)*w13-b13)))*(1-1/(1+exp(-x(i)*w13-b13))) * e(i) * w23;
-    delta4 = (1/(1+exp(-x(i)*w14-b14)))*(1-1/(1+exp(-x(i)*w14-b14))) * e(i) * w24;
-    delta5 = (1/(1+exp(-x(i)*w15-b15)))*(1-1/(1+exp(-x(i)*w15-b15))) * e(i) * w25;
-    
+    for j=1 : length(w1)
+        delta(j) = (1/(1+exp(-x(i)*w1(j)-b1(j))))*(1-1/(1+exp(-x(i)*w1(j)-b1(j)))) * e(i) * w2(j);
+    end
     % updating wieghts
-    w11 = w11 + n * delta1 * x(i);
-    w12 = w12 + n * delta2 * x(i);
-    w13 = w13 + n * delta3 * x(i);
-    w14 = w14 + n * delta4 * x(i);
-    w15 = w15 + n * delta5 * x(i);
-    
-    %updating b
-    b11 = b11 * n * delta1;
-    b12 = b12 * n * delta2;
-    b13 = b13 * n * delta3;
-    b14 = b14 * n * delta4;
-    b15 = b15 * n * delta5;
-    
+    for j=1:length(w1)
+        w1(j) = w1(j) + n * delta(j) * x(i);
+        %updating b
+        b1(j) = b1(j) * n * delta(j);
+    end
     
 end
 
